fbb40f774a54db70f454317febed5e49a5cbea77
fadgadahababashirdev <fadgadahababashirdev@gmail.com>
2023-09-27 10:38:39 +0200

    phpOop

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    class Fruit{
+
+        public $name;
+        public $model;
+        public $version;
+
+        function __construct($name , $model , $version){
+            $this->name = $name;
+            $this->model = $model;
+            $this->version = $version;
+        }
+    }
+
+    $name = new Fruit("bmw" , "Toyota" , "2020");
+
+    $name->
+    
+    ?>
+    
+</body>
+</html>
